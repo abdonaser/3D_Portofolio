dde766bb4daadf334b4b853edf0402a01b26884f
abdonaser <abdonaser4223@gmail.com>
2024-11-21 03:35:05 +0200

    Projects-1

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -73,7 +73,7 @@ const Works = () => {
       <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3lg leading-[3.px]">
+          className="mt-3 text-secondary text-[17px] max-w-3lg leading-[3.px] bg-red-100">
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
